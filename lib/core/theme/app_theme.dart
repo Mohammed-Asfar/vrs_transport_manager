2705afed421bc55df3198000cbf4d946b0cd52dd
Mohammed-Asfar <asfarmohammed49@gmail.com>
2026-03-15 15:38:50 +0530

    first commit

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    colorSchemeSeed: AppColors.primaryColor,
+    useMaterial3: true,
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:vrs_transport_manager/features/home/presentation/pages/home_page.dart';
+import 'core/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const HomePage(), theme: AppTheme.lightTheme);
+  }
+}
